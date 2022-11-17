e69ed3657e6c94c1abc57f782f4fb9520a11a24b
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-17 20:18:35 -0300

    requisito 3

--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
 import { searchCep } from './helpers/cepFunctions';
+import { fetchProductsList } from './helpers/fetchFunctions';
+import { createProductElement } from './helpers/shopFunctions';
 import './style.css';
 
 document.querySelector('.cep-button').addEventListener('click', searchCep);
+
+const productsLista = document.querySelector('.products');
+
+const addProdutos = async () => {
+  const produtos = await fetchProductsList('computador');
+  produtos.forEach((produto) => {
+    const { id, title, thumbnail, price } = produto;
+    const lista = createProductElement({ id, title, thumbnail, price });
+    productsLista.appendChild(lista);
+  });
+};
+
+window.onload = () => {
+  addProdutos();
+};
