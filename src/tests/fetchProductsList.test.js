30b1f14fc619266e77d10f6693e8191c6e234b33
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-11-18 11:49:48 -0300

    requisito 1 refatorando

--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -1,27 +1,32 @@
 import './mocks/fetchSimulator';
 import { fetchProductsList } from '../helpers/fetchFunctions';
-import computadorSearch from './mocks/search';
+import { computadorSearch } from './mocks/search';
 
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
+
   it('Teste se é uma função', () => {
     expect(fetchProductsList).toBeInstanceOf(Function);
   });
+
   it('Execute a função fetchProductsList com o argumento computador e teste se fetch foi chamada;', async () => {
     await fetchProductsList('computador');
     expect(fetch).toHaveBeenCalled();
   });
+
   it('Teste se, ao chamar a função fetchProductsList com o argumento computador, a função fetch utiliza o endpoint ', async () => {
     await fetchProductsList('computador');
     expect(fetch).toHaveBeenCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador')
   });
-  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
-    await fetchProductsList('computador').then((data) => {
+
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', () => {
+     fetchProductsList('computador').then((data) => {
       expect(data).toEqual(computadorSearch);
     })
   });
+  
   it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: Termo de busca não informado.', () => {
-    expect(fetchProductsList()).rejects.toThrow(error('Termo de busca não informado'));
+    expect(fetchProductsList()).rejects.toThrow(Error('Termo de busca não informado'));
 
   });
 });
